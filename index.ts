3870d6193600b502fafdb01d586540babbdaf4e6
hideo54 <hideo54@hideo54.com>
2020-09-13 02:37:00 +0900

    Make window transparent and frame

--- index.ts
+++ index.ts
@@ -7,7 +7,12 @@ const meetUrl = process.env.MEET_URL!;
 const createWindow = () => {
     const width = 1600;
     const height = width * 9 / 16;
-    const win = new BrowserWindow({ width, height });
+    const win = new BrowserWindow({
+        width, height,
+        transparent: true,
+        frame: false,
+    });
+    win.show();
     win.loadURL(meetUrl);
 };
 
