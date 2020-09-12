a590f3cffe974cfd0c675b84ba2a8c7a24891d6a
hideo54 <hideo54@hideo54.com>
2020-09-13 02:34:02 +0900

    すべてを壊した

--- index.ts
+++ index.ts
@@ -1,34 +1,14 @@
-import { app, BrowserWindow, BrowserView } from 'electron';
+import { app, BrowserWindow } from 'electron';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const googleMeetUrl = process.env.GOOGLE_MEET_URL!;
+const meetUrl = process.env.MEET_URL!;
 
 const createWindow = () => {
     const width = 1600;
     const height = width * 9 / 16;
-    const leftViewWidth = height * 4 / 3;
     const win = new BrowserWindow({ width, height });
-
-    const meetView = new BrowserView();
-    win.addBrowserView(meetView);
-    meetView.setBounds({
-        x: 0,
-        y: 0,
-        width: leftViewWidth,
-        height,
-    });
-    meetView.webContents.loadURL(googleMeetUrl);
-
-    const bannerView = new BrowserView();
-    win.addBrowserView(bannerView);
-    bannerView.setBounds({
-        x: leftViewWidth,
-        y: 0,
-        width: width - leftViewWidth,
-        height,
-    });
-    bannerView.webContents.loadFile('banner/index.html');
+    win.loadURL(meetUrl);
 };
 
 app.whenReady().then(createWindow);
