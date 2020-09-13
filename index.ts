0d7c9f68c8a9d0f38db3ed4fb10cbc4f2f38f56b
hideo54 <hideo54@hideo54.com>
2020-09-13 17:04:19 +0900

    Implement focus command server and features

--- index.ts
+++ index.ts
@@ -4,7 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const meetUrl = process.env.MEET_URL!;
+const focusJS = fs.readFileSync('./focus.js', 'utf-8');
 const customCSS = fs.readFileSync('./style.css', 'utf-8');
+
+import fastify from 'fastify';
+
+const server = fastify({ logger: true });
 
 const createWindow = () => {
     const width = 1000;
@@ -14,12 +19,37 @@ const createWindow = () => {
         title: 'TSG LIVE!',
         transparent: true,
         frame: false,
+        webPreferences: {
+            preload: focusJS,
+        },
     });
     win.loadURL(meetUrl);
     win.show();
     const contents = win.webContents;
     contents.on('did-finish-load', () => {
         contents.insertCSS(customCSS);
+        setTimeout(async () => {
+            contents.executeJavaScript(focusJS);
+            server.get('/focus', async (requeset, reply) => {
+                const name = await contents.executeJavaScript('getCurrentFocus()');
+                reply.send({ name });
+            });
+            server.post<{
+                Params: {
+                    name?: string;
+                };
+            }>('/focus/:name', async (request, reply) => {
+                const name = request.params.name;
+                try {
+                    await contents.executeJavaScript(`focus('${name}')`);
+                    reply.send({ ok: true });
+                } catch (e) {
+                    reply.send({ ok: false, error: e });
+                }
+            });
+            server.listen(25252);
+            console.log('Focus handling preparation done');
+        }, 10 * 1000); // 10s
     });
 };
 
