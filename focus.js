5b50240209b64a18143bea42f3b5bf7379cb0361
hideo54 <hideo54@hideo54.com>
2020-09-13 17:34:14 +0900

    Fix choice of commiting files

---
+++ focus.js
@@ -0,0 +1,25 @@
+const containers = Array.from(document.getElementsByClassName('videocontainer'));
+const participants = containers.filter(e => e.id.startsWith('participant'));
+const participantIds = participants.map(p => p.id);
+const participantNames = participants.map(p => document.getElementById(p.id + '_name').innerHTML);
+let nameToId = {};
+let idToName = {};
+for (let i = 0; i < participants.length; i++) {
+    const name = participantNames[i].toLowerCase();
+    const id = participantIds[i];
+    nameToId[name] = id;
+    idToName[id] = name;
+}
+
+const getCurrentFocus = () => {
+    const id = participants.filter(e => Array.from(e.classList).includes('videoContainerFocused'))[0].id;
+    const name = idToName[id];
+    return name;
+};
+
+const focus = name => {
+    const currentSpeakerName = getCurrentFocus();
+    if (name !== currentSpeakerName) {
+        document.getElementById(nameToId[name]).click();
+    }
+};
