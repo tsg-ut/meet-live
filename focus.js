cab366371a7fc843ad1f54d270fa93a50e7b8873
hideo54 <hideo54@hideo54.com>
2020-09-15 03:50:20 +0900

    Improve injected JS

--- focus.js
+++ focus.js
@@ -1,17 +1,25 @@
-const containers = Array.from(document.getElementsByClassName('videocontainer'));
-const participants = containers.filter(e => e.id.startsWith('participant'));
-const participantIds = participants.map(p => p.id);
-const participantNames = participants.map(p => document.getElementById(p.id + '_name').innerHTML);
+let participants = [];
+let participantIds = [];
+let participantNames = [];
 let nameToId = {};
 let idToName = {};
-for (let i = 0; i < participants.length; i++) {
-    const name = participantNames[i].toLowerCase();
-    const id = participantIds[i];
-    nameToId[name] = id;
-    idToName[id] = name;
-}
+
+const update = () => {
+    participants = Array.from(
+            document.getElementsByClassName('videocontainer')
+        ).filter(e => e.id.startsWith('participant'));
+    participantIds = participants.map(p => p.id);
+    participantNames = participants.map(p => document.getElementById(p.id + '_name').innerHTML);
+    for (let i = 0; i < participants.length; i++) {
+        const name = participantNames[i].toLowerCase();
+        const id = participantIds[i];
+        nameToId[name] = id;
+        idToName[id] = name;
+    }
+};
 
 const getCurrentFocus = () => {
+    update();
     const id = participants.filter(e => Array.from(e.classList).includes('videoContainerFocused'))[0].id;
     const name = idToName[id];
     return [ name, participantNames ];
