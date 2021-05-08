155d48f66832b80b57667c4280a42eeadc57182d
Tatsuyuki Ishi <ishitatsuyuki@gmail.com>
2021-05-08 15:06:45 +0900

    Fix sharing area size and background throttling

--- index.ts
+++ index.ts
@@ -26,6 +26,7 @@ const createWindow = () => {
         frame: false,
         webPreferences: {
             preload: focusJS,
+            backgroundThrottling: false,
         },
     });
     win.loadURL(meetUrl);
